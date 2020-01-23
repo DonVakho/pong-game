24a1aefae68ed0303827f7e6be98505e3b876a57
vdonadze <vakho159@gmail.com>
2020-01-23 18:20:07 +0100

    initial commit

---
+++ desktop/src/com/ntnu/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ntnu.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ntnu.game.PongGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PongGame(), config);
+	}
+}
